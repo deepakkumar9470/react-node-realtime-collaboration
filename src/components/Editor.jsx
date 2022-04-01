568589f13a0c236cfcadf362290465d8fdf99d2d
Deepak Kumar Chouhan <dk397787@gmail.com>
2022-04-01 12:24:22 +0530

    react node collaboration

---
+++ src/components/Editor.jsx
@@ -0,0 +1,72 @@
+import React,{useEffect,useRef} from 'react'
+
+import Codemirror from 'codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/dracula.css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/addon/edit/closetag'
+import 'codemirror/addon/edit/closebrackets'
+
+const Editor = ({roomId, socketRef, onCodeChange}) => {
+  
+    const editorRef  = useRef()
+
+  useEffect(() => {
+      
+     async function init(){
+        editorRef.current = Codemirror.fromTextArea(document.getElementById('realtimeText'), {
+          mode : {name : 'javascript', json:  true},
+          theme:  'dracula',
+          autoCloseTags: true,
+          autoCloseBrackets: true,
+          lineNumbers : true
+        });
+
+        editorRef.current.on('change', (instance, changes) => {
+          console.log('changes', changes)
+          const {origin} = changes
+          const code = instance.getValue()
+          onCodeChange(code)
+          if(origin !== 'setValue'){
+            socketRef.current.emit('code-change',{
+              roomId,
+              code
+            })
+          }
+          console.log(code)
+        });
+ 
+     }
+        
+        
+    
+     init()
+      
+  }, []);
+  
+  useEffect(() => {
+
+    if(socketRef.current){
+
+      socketRef.current.on('code-change', ({code})=>{
+        if(code !== null){
+          editorRef.current.setValue(code)
+        }
+      })
+    }
+    
+    return () =>{
+      socketRef.current.off('code-change')
+    }
+  
+    
+  }, [socketRef.current])
+  
+
+
+  return (
+    <textarea  id="realtimeText"></textarea>
+  )
+}
+
+export default Editor
